8e9803a656bb6f9ebec7a283ee28f17e703901dc
nicolito <nicolito1570@gmail.com>
2026-05-18 22:59:51 -0400

    init spring proyect

---
+++ Backend/cronicotrak/src/main/java/iye/grupo2/cronicotrak/CronicotrakApplication.java
@@ -0,0 +1,13 @@
+package iye.grupo2.cronicotrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronicotrakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronicotrakApplication.class, args);
+	}
+
+}
---
+++ Backend/cronicotrak/src/test/java/iye/grupo2/cronicotrak/CronicotrakApplicationTests.java
@@ -0,0 +1,13 @@
+package iye.grupo2.cronicotrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronicotrakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
